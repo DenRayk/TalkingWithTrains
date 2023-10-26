385b15a3a994aaa3dc74e52a82b7497038f8e54e
DenRay <drayker@gmx.de>
2023-10-26 11:12:18 +0200

    Add vosk model

---
+++ comparison/vosk/main_vosk.py
@@ -0,0 +1,58 @@
+import speech_recognition as sr
+import sys
+
+keyword = ["merklin", "märklin", "merkel"]
+
+recognizer = sr.Recognizer()
+
+
+def speech_to_text(audio):
+    text = recognizer.recognize_vosk(audio, language="de-DE")
+    return text.lower()
+
+
+def listen_for_wakeword():
+    while True:
+
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source, duration=0.2)
+            print("Listening for wakeword...")
+            audio = recognizer.listen(source)
+
+        try:
+
+            text = speech_to_text(audio)
+            print("You said: {}".format(text))
+
+            if text in keyword:
+                listen_for_command()
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+
+
+def listen_for_command():
+    with sr.Microphone() as source:
+        print("Listening for command...")
+        audio = recognizer.listen(source)
+
+        text = speech_to_text(audio)
+
+        if text is not None:
+            print("You said: {}".format(text))
+            process_command(text)
+        else:
+            print("Could not understand audio")
+
+
+def process_command(text):
+    print("Processing command...")
+    # TODO: Add code to process commands
+
+
+while True:
+    try:
+        listen_for_wakeword()
+    except KeyboardInterrupt:
+        sys.exit(0)
