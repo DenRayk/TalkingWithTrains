d115dee9d85eeff7000fb5bed1df0499894daad1
DenRay <drayker@gmx.de>
2023-11-29 23:26:53 +0100

    Add multiple cases to call

--- speech_control/activities.py
+++ speech_control/activities.py
@@ -16,6 +16,7 @@ def extract_activity(prompt_text):
     prompt_text_json = json.loads(prompt_text)
 
     match prompt_text_json["text"]:
+
         # System Befehle bisher nicht in der API
         case "system start":
             drive_all()
@@ -23,31 +24,31 @@ def extract_activity(prompt_text):
             stop_all()
 
         # Züge Richtung
-        case "fahre zug eins geradeaus":
+        case "fahre zug eins geradeaus" | "fahre zug eins vorwärts" | "zug eins fahre geradeaus" | "zug eins fahre vorwärts":
             http_client.set_train_direction("zug_1", "Forwards")
-        case "fahre zug eins zurück":
+        case "fahre zug eins rückwärts" | "zug eins fahre rückwärts":
             http_client.set_train_direction("zug_1", "Backwards")
-        case "fahre zug zwei geradeaus":
+        case "fahre zug zwei geradeaus" | "fahre zug zwei vorwärts" | "zug zwei fahre geradeaus" | "zug zwei fahre vorwärts":
             http_client.set_train_direction("zug_2", "Forwards")
-        case "fahre zug zwei zurück":
+        case "fahre zug zwei rückwärts" | "zug zwei fahre rückwärts":
             http_client.set_train_direction("zug_2", "Backwards")
-        case "fahre zug drei geradeaus":
+        case "fahre zug drei geradeaus" | "fahre zug drei vorwärts" | "zug drei fahre geradeaus" | "zug drei fahre vorwärts":
             http_client.set_train_direction("zug_3", "Forwards")
-        case "fahre zug drei zurück":
+        case "fahre zug drei rückwärts" | "zug drei fahre rückwärts":
             http_client.set_train_direction("zug_3", "Backwards")
-        case "fahre zug vier geradeaus":
+        case "fahre zug vier geradeaus" | "fahre zug vier vorwärts" | "zug vier fahre geradeaus" | "zug vier fahre vorwärts":
             http_client.set_train_direction("zug_4", "Forwards")
-        case "fahre zug vier zurück":
+        case "fahre zug vier rückwärts" | "zug vier fahre rückwärts":
             http_client.set_train_direction("zug_4", "Backwards")
 
         # Züge anhalten
-        case "zug eins stop":
+        case "zug eins stop" | "zug eins halte an":
             http_client.set_train_speed("zug_1", 0)
-        case "zug zwei stop":
+        case "zug zwei stop" | "zug zwei halte an":
             http_client.set_train_speed("zug_2", 0)
-        case "zug drei stop":
+        case "zug drei stop" | "zug drei halte an":
             http_client.set_train_speed("zug_3", 0)
-        case "zug vier stop":
+        case "zug vier stop" | "zug vier halte an":
             http_client.set_train_speed("zug_4", 0)
 
         case _:
