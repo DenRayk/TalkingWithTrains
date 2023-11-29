eaf14e80e887c20d0f51a7b3b110972288b125c3
DenRay <drayker@gmx.de>
2023-11-29 23:05:03 +0100

    Add config.py file

---
+++ config.py
@@ -0,0 +1,48 @@
+base_url = "http://127.0.0.1:8042"
+x_can_hash = None
+
+trains = {
+    "zug_1": 16391,
+    "zug_2": 16389,
+    "zug_3": 16392,
+    "zug_4": 16390,
+}
+
+functions_crossrail = {
+    "Licht": 1,
+    "Rauch": 2,
+    "Betriebsgeräusche": 3,
+    "Hupe 1": 4,
+    "Gewicht": 5,
+    "Anfahrgeräusche": 6,
+    "Innenlicht": 7,
+    "Hupe 2": 8,
+    "Innenlicht / Vorne": 9,
+    "Lüftungsgeräusche": 10,
+    "Kück - Sound": 11,
+    "Kompressor": 12,
+    "Licht unten": 13,
+    "Licht unten 2": 14,
+    "Fake Anfahrgeräusche": 15,
+    "Hupe 3": 16,
+    "Hupe 4": 17,
+    "Turtle": 18,
+    "Kleines Licht": 19,
+    "Großes Licht": 20,
+    "Luft ablassen": 21,
+    "Motorgeräusche": 22,
+    "Abkoppelsound 1": 23,
+    "FULL Sound": 24,
+    "Abkoppelsound 2": 25,
+    "ALARM 1": 26,
+    "ALARM 2": 27,
+    "Türen": 28
+}
+
+functions_drg = {
+    "Licht": 1,
+    "Rauch": 2,
+    "Laggy Betriebsgeräusche": 3
+}
+
+
